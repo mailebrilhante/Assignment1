8aa01b7ffeddcef20b98654835d13ebb838a05cb
mailebrilhante <155116907+mailebrilhante@users.noreply.github.com>
2025-06-28 12:22:23 -0600

    Added Line class

---
+++ src/main/kotlin/Line.kt
@@ -0,0 +1,28 @@
+import org.example.Point
+import kotlin.math.sqrt
+
+class Line(private val start: Point, private val end: Point) {
+
+    fun getStart() = start
+
+    fun getEnd() = end
+
+    fun getLength(): Double {
+        val deltax = end.x - start.x
+        val deltay = end.y - start.y
+        return sqrt(deltax * deltax + deltay * deltay)
+    }
+
+    fun getSlope() = (end.y - start.y) / (end.x - start.x)
+
+    fun setMoveStrategy(newMove: Move){
+        start.setMoveStrategy(newMove)
+        end.setMoveStrategy(newMove)
+    }
+
+    fun moveLine(){
+        start.movePoint()
+        end.movePoint() 
+    }
+
+}
