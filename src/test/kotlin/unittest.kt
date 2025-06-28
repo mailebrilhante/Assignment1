921c74b019491de10f09a8582cec697d34e04354
mailebrilhante <155116907+mailebrilhante@users.noreply.github.com>
2025-06-28 12:53:09 -0600

    Added unit tests for Line class functions

--- src/test/kotlin/unittest.kt
+++ src/test/kotlin/unittest.kt
@@ -7,16 +7,66 @@ class TestPoint {
     @Test
     fun testMove() {
         val point = Point(0.0, 0.0, MoveStraight(1.0, 1.0))
-        val newPoint = point.move()
-        assertEquals(1.0, newPoint.x)
-        assertEquals(1.0, newPoint.y)
+        point.movePoint()
+        assertEquals(1.0, point.x)
+        assertEquals(1.0, point.y)
+    }
+
+    @Test
+    fun testSetMoveStrategy() {
+        val point = Point(0.0, 0.0, null)
+        point.setMoveStrategy(MoveStraight(1.0, 1.0))
+        point.movePoint()
+        assertEquals(1.0, point.x)
+        assertEquals(1.0, point.y)
     }
 
     @Test
     fun testClonePoint() {
         val point = Point(0.0, 0.0, null)
         val newPoint = point.clonePoint()
-        assertEquals(0.0, newPoint.x)
-        assertEquals(0.0, newPoint.y)
+        assertEquals(point.x, newPoint.x)
+        assertEquals(point.y, newPoint.y)
+    }
+
+    @Test
+    fun testGetLength(){
+        val point1 = Point(0.0, 0.0, null)
+        val point2 = Point(1.0, 1.0, null)
+        val line = Line(point1, point2)
+        assertEquals(Math.sqrt(2.0), line.getLength())
+    }
+
+    @Test
+    fun testGetSlope() {
+        val point1 = Point(0.0, 0.0, null)
+        val point2 = Point(1.0, 1.0, null)
+        val line = Line(point1, point2)
+        assertEquals(1.0, line.getSlope())
+    }
+
+    @Test
+    fun testMoveLine(){
+        val point1 = Point(0.0, 0.0, MoveStraight(1.0, 1.0))
+        val point2 = Point(1.0, 1.0, MoveStraight(1.0, 1.0))
+        val line = Line(point1, point2)
+        line.moveLine()
+        assertEquals(1.0, line.getStart().x)
+        assertEquals(1.0, line.getStart().y)
+        assertEquals(2.0, line.getEnd().x)
+        assertEquals(2.0, line.getEnd().y)
+    }
+
+    @Test
+    fun testSetMoveStrategyLine() {
+        val point1 = Point(0.0, 0.0, null)
+        val point2 = Point(1.0, 1.0, null)
+        val line = Line(point1, point2)
+        line.setMoveStrategy(MoveStraight(1.0, 1.0))
+        line.moveLine()
+        assertEquals(1.0, line.getStart().x)
+        assertEquals(1.0, line.getStart().y)
+        assertEquals(2.0, line.getEnd().x)
+        assertEquals(2.0, line.getEnd().y)
     }
 }
