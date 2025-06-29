79021dfeb923182017bfa0ada01730dd159d797d
mailebrilhante <155116907+mailebrilhante@users.noreply.github.com>
2025-06-28 20:30:06 -0600

    Etended Shape class with Ellipse implementation

---
+++ src/main/kotlin/Ellipse.kt
@@ -0,0 +1,28 @@
+import org.example.Point
+
+class Ellipse(center: Point, private val _radius1: Double, private val _radius2: Double) : Shape(listOf(
+    center
+)) {
+
+    val center : Point
+        get() = points[0]
+    val radius1: Double
+        get() = _radius1
+    val radius2: Double
+        get() = _radius2
+
+    init{
+        if (radius1 <= 0 || radius2 <= 0) {
+            throw IllegalArgumentException("Radii must be positive.")
+        }
+
+    }
+    
+    override fun getAttributes(): List<Any> {
+        return listOf(center, radius1, radius2)
+    }
+
+    override fun getArea(): Double {
+        return Math.PI * radius1 * radius2
+    }
+}
